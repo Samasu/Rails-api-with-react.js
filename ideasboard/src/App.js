0073b0d30cd81edca3fce52ac0c3b8e1c07839b1
Sada <samasunari@gmail.com>
2019-06-18 03:22:18 +0900

    build rails app and react

---
+++ ideasboard/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import IdeasContainer from './components/IdeasContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Idea </h1>
+        <IdeasContainer />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ideasboard/src/components/IdeasContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class IdeasContainer extends Component {
+ render() {
+  return (
+    <div className="App">
+        <h1> Ideasboard </h1>
+    </div>
+   );
+  }
+}
+
+export default IdeasContainer;
